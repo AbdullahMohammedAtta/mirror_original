0d7c01d63d277d42b79549acf03323cfd884a13f
Abdullah Mohamed <abdullahata000@gmail.com>
2026-06-07 17:31:00 +0300

    edit box shadow

--- lib/features/profile/view/profile_page.dart
+++ lib/features/profile/view/profile_page.dart
@@ -168,7 +168,7 @@ class ProfilePage extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.02),
+            color: Colors.grey.shade300,
             blurRadius: 10,
             offset: const Offset(0, 4),
           ),
