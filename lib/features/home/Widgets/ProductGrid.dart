df5da19a5f93b2a6dcd7b8f5132ed7403a16e68d
Abdullah Mohammed Atta <abdullahata000@gmail.com>
2026-08-19 03:51:46 +0300

    trending show in home page

--- lib/features/home/Widgets/ProductGrid.dart
+++ lib/features/home/Widgets/ProductGrid.dart
@@ -3,12 +3,25 @@ import 'package:mirror_original/features/home/Widgets/ProductCard.dart';
 import 'package:mirror_original/features/home/model/product_model.dart';
 import 'package:mirror_original/features/home/view_model/home_cubit.dart';
 
-Widget buildProductGrid(List<ProductModel> products,HomeCubit homeCubit,{ScrollPhysics physics = const NeverScrollableScrollPhysics()}) {
+Widget buildProductGrid(
+    List<ProductModel> products,
+    HomeCubit homeCubit, {
+      ScrollPhysics physics = const NeverScrollableScrollPhysics(),
+    }) {
+  // Find Trending category
+  final trendingCategory = homeCubit.categories.firstWhere(
+        (category) => category.name == 'Trending',
+  );
+
+  // Get only products that belong to Trending
+  final trendingProducts = products
+      .where((product) => product.categoryId == trendingCategory.id)
+      .toList();
 
   return GridView.builder(
     shrinkWrap: true,
     physics: physics,
-    itemCount: products.length,
+    itemCount: trendingProducts.length,
     gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
       maxCrossAxisExtent: 220,
       childAspectRatio: 0.56,
@@ -16,7 +29,11 @@ Widget buildProductGrid(List<ProductModel> products,HomeCubit homeCubit,{ScrollP
       mainAxisSpacing: 10,
     ),
     itemBuilder: (context, index) {
-      return buildProductCard(products[index],homeCubit,context);
+      return buildProductCard(
+        trendingProducts[index],
+        homeCubit,
+        context,
+      );
     },
   );
 }
